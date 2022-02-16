9c00172c112c7adf14d2872426fe0302086e90df
ayush20222 <99115140+ayush20222@users.noreply.github.com>
2022-02-16 20:15:57 +0530

    Update voice_assitant.py

--- voice_assitant.py
+++ voice_assitant.py
@@ -1,1 +1,13 @@
-
+STEPS TO USE ..  
+  1. Open reddit subreddit : Opens the subreddit in default browser.
+  2. Open xyz.com : replace xyz with any website name
+  3. Send email/email : Follow up questions such as recipient name, content will be asked in order.
+  4. Tell a joke/another joke : Says a random dad joke.
+  5. Current weather in {cityname} : Tells you the current condition and temperture
+  6. Hello
+  7. play me a video : Plays song in your VLC media player
+  8. change wallpaper : Change desktop wallpaper
+  9. news for today : reads top news of today
+  10. time : Current system time
+  11. top stories from google news (RSS feeds)
+  12. tell me about xyz : tells you about xyz
